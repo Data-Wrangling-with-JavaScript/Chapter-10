dc3fe8743728795802999c167589df01100b5879
Ashley Davis <ashley@codecapers.com.au>
2018-02-07 08:04:21 +1000

    Added listing 5.

---
+++ listing-5/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const importCsvFile = require('./toolkit/importCsvFile.js');
+
+const app = express();
+
+const staticFilesPath = path.join(__dirname, "public");
+const staticFilesMiddleWare = express.static(staticFilesPath);
+app.use('/', staticFilesMiddleWare);
+
+app.get('/rest/data', (request, response) => {
+
+    importCsvFile("./data/combined-yearly-temperature.csv")
+        .then(data => {
+            response.json(data);
+        })
+        .catch(err => {
+            console.error(err);
+
+            response.sendStatus(500);
+        });
+});
+
+app.listen(3000, () => {
+    console.log("Web server listening on port 3000!");
+});
---
+++ listing-5/public/app.js
@@ -0,0 +1,42 @@
+//
+// Simplest possible example of a C3 chart in a web page.
+//
+// Based on the example line chart: http://c3js.org/samples/simple_multiple.html
+//
+// To run this, first install bower  install live-server:
+//
+//      npm install -g bower
+//
+//      npm install -g live-server
+//
+// Then change directory to the same directory as this file and install dependencies:
+//
+//      bower install
+//
+// Then run live server from the same directory as this file:
+//
+//      live-server
+//
+// Your browser will open and a line chart will be rendered.
+//
+
+$(function () {
+
+    $.get("/rest/data")
+        .then(function (data) {
+            var chart = c3.generate({
+                data: {
+                    json: data,
+                    keys: {
+                        x: "Year",
+                        value: ["TempNYC", "TempLA"]
+                    }
+                }
+            });
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+
+});
+
